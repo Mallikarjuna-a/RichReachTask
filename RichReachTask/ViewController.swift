58c7342a4cb25177d9403b268862fb538398c57c
Mallikarjuna <adramallikarjuna095@gmail.com>
2021-01-02 18:01:38 +0530

    Initial Commit

---
+++ RichReachTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RichReachTask
+//
+//  Created by Mallikarjuna on 02/01/21.
+//  Copyright © 2021 Mallikarjuna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
